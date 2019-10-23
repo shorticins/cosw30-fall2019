216772a18fd1a36aa29e614db483f0ae822dc4af
GaithCattan <gaith.c@gmail.com>
2019-10-23 01:28:09 +0000

    modify version1

--- customer_service/contact_us.php
+++
@@ -1,7 +0,0 @@
-<?php include('includes/header.php'); ?>
-
-<?php include('includes/customer_service_nav_bar.php'); ?>
-
-<?php include('includes/resources/contact_us_res.php'); ?>
-
-<?php include('includes/footer.php'); ?>
