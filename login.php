270b64f309d1c75720e75779e7ccdbfb6642320b
SavantLogics <rsaviant@gmail.com>
2019-10-23 05:32:11 +0000

    Added login and registration forms

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php include('includes/header.php');?>
+<div id="page-container">
+
+    <div id="content-wrap">
+
+
+
+        <form action="login.php" method="POST">
+            <div class="container">
+
+                <h1>Login Form</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr>
+
+                <label>Email Address:</label><br>
+                <input type="email" name="email" id="email" value="<?php echo $email; ?>">
+
+                <br><label>Password:</label><br>
+                <input type="password" name="password" id="password" >
+
+                <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+                <div class="clearfix">
+                    <button name="Cancel" class="cancelbutton">Cancel</button>
+                    <button name="Login in" class="loginbutton">Log in!</button>
+                </div>
+            </div>
+        </form>
+    <?php include('includes/footer.php');?>
+    </div>
---
+++ registration.php
@@ -0,0 +1,36 @@
+<?php include('includes/header.php');?>
+<div id="page-container">
+
+    <div id="content-wrap">
+
+
+        <div class="container">
+
+            <h1>Register Today</h1>
+            <hr>
+
+            <form action="register.php" method="POST">
+
+                <label>First Name:</label><br>
+                <input type="text" name="first_name" id="first_name" value="<?php echo $first_name; ?>" >
+
+                <br><label>Last Name:</label><br>
+                <input type="text" name="last_name" id="last_name" value="<?php echo $last_name; ?>" >
+
+                <br><label>Email Address:</label><br>
+                <input type="email" name="email" id="email" value="<?php echo $email; ?>" >
+
+                <br><label>Password:</label><br>
+                <input type="password" name="password" id="password" value="<?php echo $password; ?>" >
+
+                <br><label>Confirm Password:</label><br>
+                <input type="password" name="confirm_password" id="confirm_password" value="<?php echo $confirm_password; ?>" >
+
+
+                <br><button>Register!</button>
+            </form>
+        </div>
+
+        <?php include('includes/footer.php');?>
+    </div>
+</div>
