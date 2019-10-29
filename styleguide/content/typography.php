0c7c40a1ea7dec226b5551d982ceed722a97e326
Diana <rockondiana0416@yahoo.com>
2019-10-29 20:17:50 +0000

    Move $b-font to vars, created typography.php, removed $logo & $co-var

---
+++ styleguide/content/typography.php
@@ -0,0 +1,5 @@
+<?php
+/* colors: h1 #2E9E47 (green), h2 #7b4b94 (purple), h3 #9cd0f0 (light blue), h4 #b81f24 (dark red), h5 #d3d3d3 (light gray), h6 #000 (black)
+fonts added with tag in html head + css font-family (note: logo font is not available on Google fonts and will use system sans-seriff default)
+<link href= "https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet"> */
+?>
