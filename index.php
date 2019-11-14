f427c47d947ce9b1cfd704251438e555e5d5d235
developergrace <gpark2351@gmail.com>
2019-11-14 03:19:25 +0000

    Updated index.php - GP

--- index.php
+++ index.php
@@ -4,25 +4,34 @@ include('model/product.php');
 ?>
 
 <main class="container">
-    <h1>This will be the homepage of the class project website</h1>
+    <div class="hero">
+        <img src="/" alt="Treat Street Image">
+    </div>
 
-    <p><a href="./styleguide">Temporary link to the styleguide</a></p>
+    <h2>Product Categories</h2>
+    <div class="category_section">
+        <div class="category_item">
+            <img src="/img/bone_pile.png" alt="Food Picture">
+            <h3>Food</h3>
+            <p>Lorem ipsum dolor amet tote bag meggings echo park, food truck XOXO man bun sriracha. Synth shabby chic banjo pour-over. Scenester XOXO plaid viral, jean shorts actually YOLO cornhole. Hoodie fanny pack stumptown, shoreditch jean shorts trust fund glossier authentic unicorn four dollar toast celiac sartorial bicycle rights shabby chic.</p>
+            <a class="btn btn--green">See More</a>
+        </div>
 
-    <h3>Example of Creating a List Using Database Data</h3>
-    <?php
-    // Example of how to use a database function found in the model folder
-    $products = getProducts();
+        <div class="category_item">
+            <img src="/img/bone_pile.png" alt="Toy Picture">
+            <h3>Toys</h3>
+            <p>Lorem ipsum dolor amet tote bag meggings echo park, food truck XOXO man bun sriracha. Synth shabby chic banjo pour-over. Scenester XOXO plaid viral, jean shorts actually YOLO cornhole. Hoodie fanny pack stumptown, shoreditch jean shorts trust fund glossier authentic unicorn four dollar toast celiac sartorial bicycle rights shabby chic.</p>
+            <a class="btn btn--green">See More</a>
+        </div>
 
-    if(is_array($products)) {
-        echo "<ul>";
-        foreach($products as $product) {
-            echo "<li>".$product['product_name']."</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo $products;
-    }
-    ?>
+        <div class="category_item">
+            <img src="/img/Starwars_petcostume.jpg" alt="Costume Picture">
+            <h3>Costumes</h3>
+            <p>Lorem ipsum dolor amet tote bag meggings echo park, food truck XOXO man bun sriracha. Synth shabby chic banjo pour-over. Scenester XOXO plaid viral, jean shorts actually YOLO cornhole. Hoodie fanny pack stumptown, shoreditch jean shorts trust fund glossier authentic unicorn four dollar toast celiac sartorial bicycle rights shabby chic.</p>
+            <a class="btn btn--green">See More</a>
+        </div>
+    </div>
+    
 </main>
 
 <?php include('includes/footer.php'); ?>
