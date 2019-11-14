4bf3f958aa6b6e330d7a60d7f6cfa46b16f10dc1
sunkim96 <viviankim96@gmail.com>
2019-11-14 04:38:09 +0000

    updated footer css

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,25 @@
 <!-- START FOOTER -->
+    <div class="row">
+    
+<p><img src="/admin/images/treatstreet_logo.png" id="footerimg"></p><!--image logo-->
+    </div>
     <footer class="container">
         <div class="row">
-            <a href="/" class="one column"><i class="far fa-envelope"></i></a>
-            <a href="/" class="one column"><i class="fas fa-mobile-alt"></i></a>
-            <a href="/" class="one column"><i class="fas fa-question-circle"></i></a>
-            <a href="/" class="one column"><i class="fas fa-user-circle"></i></a>
-            <p style="background-color: #2E9E47" class="four columns">Treat Street</p><!--replace with image-->
-            <a href="/" class="one column"><i class="fab fa-facebook"></i></a>
-            <a href="/" class="one column"><i class="fab fa-twitter"></i></a>
-            <a href="/" class="one column"><i class="fab fa-instagram"></i></a>
-            <a href="/" class="one column"><i class="fas fa-map-marker-alt"></i></a>
+            <a href="/" class="one column" id="social"><i class="far fa-envelope"></i></a>
+            <a href="/" class="one column" id="social"><i class="fas fa-mobile-alt"></i></a>
+            <a href="/" class="one column" id="social"><i class="fas fa-question-circle"></i></a>
+            <a href="/" class="one column" id="social"><i class="fas fa-user-circle"></i></a>
+            <a href="/" class="one column" id="social"><i class="fab fa-facebook"></i></a>
+            <a href="/" class="one column" id="social"><i class="fab fa-twitter"></i></a>
+            <a href="/" class="one column" id="social"><i class="fab fa-instagram"></i></a>
+            <a href="/" class="one column" id="social"><i class="fas fa-map-marker-alt"></i></a>
         </div>
         <div class="row">
-            <a href="/" class="one column">About</a>
-            <a href="/" class="one column">News</a>
-            <a href="/" class="one column">Blog</a>
-            <a href="/" class="one column">Careers</a>
-            <a href="/" class="one column">Contact</a>
+            <a href="/" class="one column" id="footerlink">About</a>
+            <a href="/" class="one column" id="footerlink">News</a>
+            <a href="/" class="one column" id="footerlink">Blog</a>
+            <a href="/" class="one column" id="footerlink">Careers</a>
+            <a href="/" class="one column" id="footerlink">Contact</a>
         </div>
     </footer>
 </body>
