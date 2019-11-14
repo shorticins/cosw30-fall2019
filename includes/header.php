bfb129f1a25f978db94f224dc7f5d72517627507
bjpettigrew <bjpettigrew@gmail.com>
2019-11-14 03:20:31 +0000

    nav

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,25 @@
 <head>
     <meta charset="utf-8">
     <title>Treat Street</title>
+<style>
+<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/carlito" type="text/css"/>
+p .logo_font {
+   font-family: 'CarlitoRegular';
+   font-weight: normal;
+   font-style: normal;
+}
+
+
+
+</style>
 </head>
+
+<ul>
+    <li><a href="plp.php"><p class="logo_font">doghouse</p></a></li>
+    <li><a href=""><p class="logo_font">barkery blvd</p></a></li>
+    <li><a href=""><p class="logo_font">accessory ave</p></a></li>
+    <li><a href=""><p class="logo_font">rescue road</a></p></li>
+<ul>
+
 <body>
 <!-- END HEADER -->
