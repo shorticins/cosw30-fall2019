a2e0dd7a10a2151c76859db2bd12f683fb64c485
bjpettigrew <bjpettigrew@gmail.com>
2019-11-14 04:25:35 +0000

    header styling

--- includes/header.php
+++ includes/header.php
@@ -3,33 +3,41 @@
 <head>
     <meta charset="utf-8">
     <title>Treat Street</title>
-<style>
-<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/carlito" type="text/css"/>
-p .logo_font {
-   font-family: 'CarlitoRegular';
-   font-weight: normal;
-   font-style: normal;
-}
-
-
-
-</style>
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/carlito" type="text/css"/>
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,700i&display=swap" rel="stylesheet">
     <link href="/styleguide/styles.css" rel="stylesheet">
+    <style>
+    .logo_font {
+    font-family: 'CarlitoRegular';
+    font-weight: bold;
+    font-style: normal;
+    color: white;
+    text-align: center;
+    }
+    .nav_li {
+        float: left;
+    }
+   .nav_li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        background-color: #59A472;
+        width: 100px;
+        border: 5px solid white;
+        border-radius: 5px;
+        font-family: 'CarlitoRegular';
+    }
+    </style>
     <script src="https://kit.fontawesome.com/b25da52a36.js" crossorigin="anonymous"></script>
 </head>
 
-<ul>
-    <li><a href="plp.php"><p class="logo_font">doghouse</p></a></li>
-    <li><a href=""><p class="logo_font">barkery blvd</p></a></li>
-    <li><a href=""><p class="logo_font">accessory ave</p></a></li>
-    <li><a href=""><p class="logo_font">rescue road</a></p></li>
-<ul>
 
 <body>
-    <header class="container">
+    <!--<header class="container">
         <div class="row">
-            <h2 style="background-color: #2E9E47" class="three columns">Treat Street</h2><!--replace with image-->
+            <h2 style="background-color: #2E9E47" class="three columns">Treat Street</h2>
             <form action="/" method="GET" class="eight columns">
                 <input id="search" type="text" placeholder="Search">
                 <input id="submit" type="submit" value="Search">
@@ -42,14 +50,20 @@ p .logo_font {
         </div>
         <div class="row">
             <p class="twelve columns">Your One-Stop Shop For Your Furry Friends!</p>
-        </div>
+        </div> -->
         <nav class="row">
-            <a href="/" class="two columns">Snacks</a>
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="plp.php"><p class="logo_font">doghouse</p></a></li>
+                <li class="nav_li"><a href=""><p class="logo_font">barkery blvd</p></a></li>
+                <li class="nav_li"><a href=""><p class="logo_font">accessory ave</p></a></li>
+                <li class="nav_li"><a href="" class="logo_font">rescue road</a></li>
+            </ul>
+            <!-- <a href="/" class="two columns">Snacks</a>
             <a href="/" class="two columns">Dress Up</a>
             <a href="/" class="two columns">Play Time</a>
             <a href="/" class="two columns">Health</a>
             <a href="/" class="two columns">Just For You</a>
-            <a href="/" class="two columns">Adopt</a>
+            <a href="/" class="two columns">Adopt</a> -->
         </nav>
     </header>
 <!-- END HEADER -->
