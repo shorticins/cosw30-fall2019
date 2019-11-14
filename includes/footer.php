f467e239e871b715f12f1d2c2d8ed2b777cfc537
sunkim96 <viviankim96@gmail.com>
2019-11-14 05:36:34 +0000

    updated footer image

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 <!-- START FOOTER -->
-    <div class="row">
-    
+    <div class="row" style="position: relative; left: 0; top: 0;">
+<p><img src="includes/road.png" id="footerroad" style="width: 100%;, height:100px;" ></p>
 <p><img src="/admin/images/treatstreet_logo.png" id="footerimg"></p><!--image logo-->
     </div>
     <footer class="container">
-        <div class="row">
+        <div class="iconrow">
             <a href="/" class="one column" id="social"><i class="far fa-envelope"></i></a>
             <a href="/" class="one column" id="social"><i class="fas fa-mobile-alt"></i></a>
             <a href="/" class="one column" id="social"><i class="fas fa-question-circle"></i></a>
@@ -15,11 +15,11 @@
             <a href="/" class="one column" id="social"><i class="fas fa-map-marker-alt"></i></a>
         </div>
         <div class="row">
-            <a href="/" class="one column" id="footerlink">About</a>
-            <a href="/" class="one column" id="footerlink">News</a>
-            <a href="/" class="one column" id="footerlink">Blog</a>
-            <a href="/" class="one column" id="footerlink">Careers</a>
-            <a href="/" class="one column" id="footerlink">Contact</a>
+            <a href="/" class="one column" >About</a>
+            <a href="/" class="one column" >News</a>
+            <a href="/" class="one column" >Blog</a>
+            <a href="/" class="one column" >Careers</a>
+            <a href="/" class="one column" >Contact</a>
         </div>
     </footer>
 </body>
