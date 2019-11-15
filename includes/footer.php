2677964f65c994488790716b60fd5c3706fbf649
thompson-ele <ele.thompson12@gmail.com>
2019-11-15 05:40:15 +0000

    Cleaned up header CSS and adjusted some files committed in Week 12 lab

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,10 @@
 <!-- START FOOTER -->
-    <div class="row" style="position: relative; left: 0; top: 0;">
-<p><img src="includes/road.png" id="footerroad" style="width: 100%;, height:100px;" ></p>
-<p><img src="/admin/images/treatstreet_logo.png" id="footerimg"></p><!--image logo-->
+    <div class="row">
+        <p><img src="img/road.png" id="footerroad" style="width: 100%;, height:100px;" ></p>
+<!--    <p><img src="/admin/images/treatstreet_logo.png" id="footerimg"></p> -->
     </div>
     <footer class="container">
-        <div class="iconrow">
+        <div class="row">
             <a href="/" class="one column" id="social"><i class="far fa-envelope"></i></a>
             <a href="/" class="one column" id="social"><i class="fas fa-mobile-alt"></i></a>
             <a href="/" class="one column" id="social"><i class="fas fa-question-circle"></i></a>
--- includes/header.php
+++ includes/header.php
@@ -3,54 +3,35 @@
 <head>
     <meta charset="utf-8">
 
-<link rel="shortcut icon" href="includes/favicon_ts.png" type="image/png">
+    <link rel="shortcut icon" href="img/favicon_ts.png" type="image/png">
     <title>Treat Street</title>
     <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/carlito" type="text/css"/>
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,700i&display=swap" rel="stylesheet">
     <link href="/styleguide/styles.css" rel="stylesheet">
-    <style>
-    ul {
-        list-style-type: none;
-        margin: 10px;
-        padding: 3%;
-        overflow: hidden;
-        width: 100%;
-    }
-    .nav_li {
-        float: left;
-        margin-left: 2.75%;
-    }
-   .nav_li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 6px 10px;
-        text-decoration: none;
-        background-color: #59A472;
-        /*width: 95%;*/
-        border: 2.5px solid white;
-        border-radius: 4px;
-        box-shadow: 0 0 0 6px #59A472;
-        font-family: 'CarlitoRegular';
-        font-weight: bold;
-        font-size: 1.25em;
-    }
-    .sign {
-        margin-left: 1%;
-        width: 95%;
-    }
-    .row {
-        margin-left: 29%;
-        
-    }
-    </style>
     <script src="https://kit.fontawesome.com/b25da52a36.js" crossorigin="anonymous"></script>
 </head>
 
 
 <body>
-    <!--<header class="container">
-        <div class="row">
+    <header class="site-header container">
+        <nav class="row">
+            <ul class="site-header__nav">
+                <li class="site-header__nav-item">
+                    <a href="plp.php" class="btn--sign">doghouse</a>
+                </li>
+                <li class="site-header__nav-item">
+                    <a href="" class="btn--sign">barkery blvd</a>
+                </li>
+                <li class="site-header__nav-item">
+                    <a href="" class="btn--sign">accessory ave</a>
+                </li>
+                <li class="site-header__nav-item">
+                    <a href="" class="btn--sign">rescue road</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!--<div class="row">
             <h2 style="background-color: #2E9E47" class="three columns">Treat Street</h2>
             <form action="/" method="GET" class="eight columns">
                 <input id="search" type="text" placeholder="Search">
@@ -65,19 +46,5 @@
         <div class="row">
             <p class="twelve columns">Your One-Stop Shop For Your Furry Friends!</p>
         </div> -->
-        <nav class="row">
-            <ul class="nav_ul">
-                <li class="nav_li"><a href="plp.php" class="sign">doghouse</a></li>
-                <li class="nav_li"><a href="" class="sign">barkery blvd</a></li>
-                <li class="nav_li"><a href="" class="sign">accessory ave</a></li>
-                <li class="nav_li"><a href="" class="sign">rescue road</li>
-            </ul>
-            <!-- <a href="/" class="two columns">Snacks</a>
-            <a href="/" class="two columns">Dress Up</a>
-            <a href="/" class="two columns">Play Time</a>
-            <a href="/" class="two columns">Health</a>
-            <a href="/" class="two columns">Just For You</a>
-            <a href="/" class="two columns">Adopt</a> -->
-        </nav>
     </header>
 <!-- END HEADER -->
