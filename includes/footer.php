40ab17bd8b6b5b46d14058bcff4ca680ad284ca6
sunkim96 <viviankim96@gmail.com>
2019-11-18 06:02:09 +0000

    updated footer image and social media buttons

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
-<!-- START FOOTER -->
-    <div class="row" style="position: relative; left: 0; top: 0;">
-<p><img src="includes/road.png" id="footerroad" style="width: 100%;, height:100px;" ></p>
-<p><img src="/admin/images/treatstreet_logo.png" id="footerimg"></p><!--image logo-->
-    </div>
-    <footer class="container">
+//<!-- START FOOTER -->
+
+<div class="road">
+<p><img src="img/roadlogo.png" height="190px" width="100%"></p>
+</div>
+    <footer class="row" >
         <div class="iconrow">
             <a href="/" class="one column" id="social"><i class="far fa-envelope"></i></a>
             <a href="/" class="one column" id="social"><i class="fas fa-mobile-alt"></i></a>
@@ -14,7 +14,7 @@
             <a href="/" class="one column" id="social"><i class="fab fa-instagram"></i></a>
             <a href="/" class="one column" id="social"><i class="fas fa-map-marker-alt"></i></a>
         </div>
-        <div class="row">
+        <div class="about">
             <a href="/" class="one column" >About</a>
             <a href="/" class="one column" >News</a>
             <a href="/" class="one column" >Blog</a>
