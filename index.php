6200f8a84919d1ed705a8a668c6f6b347f233adb
developergrace <gpark2351@gmail.com>
2019-11-25 01:48:45 +0000

    made index.php responsive -GP

--- index.php
+++ index.php
@@ -3,78 +3,87 @@ include('includes/header.php');
 include('model/product.php');
 ?>
 
-<main class="container">
+<main>
+<div class="welcome row">
+    <h1 class="u-color--green">Welcome</h1>
+    <img src="/img/index_1.png" alt="Treat Street Welcome Dog" class="twelve columns">
+</div>
 
-    <div class="welcome">
-        <h1 class="u-color--green">Treat Street</h1>
-        <img src="/img/index_1.png" alt="Treat Street Welcome Dog">
-    </div>
+<div class="container">
 
     <h2 class="index_h2">Product Categories</h2>
+
     <div class="category row">
     
-        <div class="category_item three columns">
+        <div class="category_item">
             <div class="category_item_img"><img src="/img/bone_pile.png" alt="Puppy Bone Cake"></div>
             <h3>Food</h3>
             <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
-        <div class="category_item three columns">
+        <div class="category_item">
             <div class="category_item_img"><img src="/img/stuffed_toy.jpg" alt="Stuffed Monkey Toy"></div>
             <h3>Toys</h3>
             <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
-        <div class="category_item three columns">
+        <div class="category_item">
             <div class="category_item_img"><img src="/img/Starwars_petcostume.jpg" alt="Star Wars Dog Costume"></div>
             <h3>Costumes</h3>
             <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
             <a class="btn btn--green">See More</a>
         </div>
     </div>
+</div>
 
-    <div class="banner">
-        <p class="textLeft">Cat-astropically Low Prices</p>
-        <img src="/img/index_2.png" alt="Low Prices Cat">
-    </div>
+<div class="banner">
+    <p class="textLeft">Cat-astropically Low Prices</p>
+    <img src="/img/index_2.png" alt="Low Prices Cat">
+</div>
+
+<div class="container">
 
     <h2 class="index_h2">Suggested Products</h2>
+    
     <div class="category row">
 
-        <div class="category_item three columns">
+        <div class="category_item">
             <div class="category_item_img"><img src="/img/homemade_treats.jpg" alt="Assorted Treats"></div>
             <h3>Dog Treats</h3>
             <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
-        <div class="category_item three columns">
+        <div class="category_item">
             <div class="category_item_img"><img src="/img/dog_treat_cake.jpg" alt="Pup Cake"></div>
             <h3>Treat Cakes</h3>
             <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
-        <div class="category_item three columns">
+        <div class="category_item">
             <div class="category_item_img"><img src="/img/festive_pop.jpg" alt="Pup Pops"></div>
             <h3>Puppy Pops</h3>
             <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
             <a class="btn btn--green">See More</a>
         </div>
     </div>
+</div>
 
     <div class="banner">
         <p class="textRight">Seasonal Treats</p>
         <img src="/img/index_3.png" alt="Holiday Treats">
     </div>
 
+<div class="container">
     <div class="index_blurb">
         <h2 class="index_h2">About Treat Street</h2>
         <p>Doggo ipsum doge smol borking doggo with a long snoot for pats heckin angery woofer smol borking doggo with a long snoot for pats mlem, length boy lotsa pats.  length boy lotsa pats. Boof shooberino shibe tungg I am bekom fat many pats heckin good boys and girls, shibe much ruin diet super chub lotsa pats wow such tempt, borkdrive smol borking doggo with a long snoot for pats doing me a frighten waggy wags doge. Puggo yapper shoober heck, big ol blep. Super chub shibe the neighborhood pupper most angery pupper I have ever seen blop, pupper wow such tempt big ol pupper.</p>
         <p>Big ol much ruin diet you are doing me a frighten snoot, many pats noodle horse. Pats doggorino borking doggo doing me a frighten pupper waggy wags I am bekom fat long woofer, such treat porgo what a nice floof you are doing me the shock puggorino heck. Sub woofer vvv woofer big ol pupper, ruff very jealous pupper. You are doin me a concern shoober wow very biscit long water shoob blop waggy wags floofs, long water shoob wrinkler very taste wow maximum borkdrive. Woofer very jealous pupper long bois heck you are doin me a concern, I am bekom fat wrinkler what a nice floof, woofer very good spot maximum borkdrive. Woofer shibe ur givin me a spook puggo extremely cuuuuuute, shooberino wrinkler. smol borking doggo with a long snoot for pats lotsa pats extremely cuuuuuute. What a nice floof pats doing me a frighten super chub waggy wags, maximum borkdrive blep long woofer. blop corgo. Heckin big ol pupper wow such tempt vvv, thicc tungg. ur givin me a spook big ol pupper pupperino. Boof stop it fren fluffer borkdrive, thicc.</p>
     </div>
+</div>
 </main>
 
 <?php include('includes/footer.php'); ?>
