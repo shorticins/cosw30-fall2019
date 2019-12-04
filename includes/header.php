300fdf2c97b9b9f5a396b7aa4256116fe8bd7ff3
thompson-ele <ele.thompson12@gmail.com>
2019-12-04 03:36:57 +0000

    Removed modified header.php file from pull request

--- includes/header.php
+++ includes/header.php
@@ -8,62 +8,25 @@
     <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/carlito" type="text/css"/>
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,700i&display=swap" rel="stylesheet">
     <link href="/styleguide/styles.css" rel="stylesheet">
-
-    <style>
-    .header_buttons {
-        display: block;
-        background-color:white;
-        padding: 20px;
-        border: 5px ridge #59A472;
-        float: right;
-        margin: 3%;
-    }
-
-
-
-
-
-    ul {
-        list-style-type: none;
-        margin: 10px;
-        padding: 3%;
-        overflow: hidden;
-        width: 100%;
-    }
-    .nav_li {
-        float: left;
-        margin-left: 2.75%;
-    }
-   .nav_li a {
-        display: block;
-        color: white;
-        text-align: center;
-        padding: 6px 10px;
-        text-decoration: none;
-        background-color: #59A472;
-        /*width: 95%;*/
-        border: 2.5px solid white;
-        border-radius: 4px;
-        box-shadow: 0 0 0 6px #59A472;
-        font-family: 'CarlitoRegular';
-        font-weight: bold;
-        font-size: 1.25em;
-    }
-    .sign {
-        margin-left: 1%;
-        width: 95%;
-    }
-    .row {
-        margin-left: 29%;
-        
-    }
-    </style>
     <script src="https://kit.fontawesome.com/b25da52a36.js" crossorigin="anonymous"></script>
 </head>
 
 
 <body>
+    
     <header class="site-header container">
+
+        <div class="site-header__top-btn">
+            <a href=""><i class="fas fa-dog fa-2x""></i></a>
+        </div>
+        <div class="site-header__top-btn">
+            <a href=""><i class="fas fa-shopping-cart fa-2x""></i></a>
+        </div>
+
+        <div class="site-header__main-logo">
+            <img src="img/white_background_logo.png" class="site-header__logo" alt="treatstreet logo">
+        </div>
+
         <nav class="row">
             <ul class="site-header__nav">
                 <li class="site-header__nav-item">
@@ -81,47 +44,5 @@
             </ul>
         </nav>
 
-        <!--<div class="row">
-            <h2 style="background-color: #2E9E47" class="three columns">Treat Street</h2>
-            <form action="/" method="GET" class="eight columns">
-                <input id="search" type="text" placeholder="Search">
-                <input id="submit" type="submit" value="Search">
-            </form>
-            <span class="one column">
-                <a href="/"><i class="fas fa-cart-plus"></i></a>
-                <a href="/"><i class="fas fa-question-circle"></i></a>
-                <a href="/login.php"><i class="fas fa-user-circle"></i>Login</a>
-            </span>
-        </div>
-        <div class="row">
-            <p class="twelve columns">Your One-Stop Shop For Your Furry Friends!</p>
-        </div> -->
-
-        <div class="header_buttons">
-
-        </div>
-        <div class = "header_buttons">
-
-        </div>
-
-
-
-
-
-
-        <nav class="row">
-            <ul class="nav_ul">
-                <li class="nav_li"><a href="plp.php" class="sign">doghouse</a></li>
-                <li class="nav_li"><a href="" class="sign">barkery blvd</a></li>
-                <li class="nav_li"><a href="" class="sign">accessory ave</a></li>
-                <li class="nav_li"><a href="" class="sign">rescue road</li>
-            </ul>
-            <!-- <a href="/" class="two columns">Snacks</a>
-            <a href="/" class="two columns">Dress Up</a>
-            <a href="/" class="two columns">Play Time</a>
-            <a href="/" class="two columns">Health</a>
-            <a href="/" class="two columns">Just For You</a>
-            <a href="/" class="two columns">Adopt</a> -->
-        </nav>
     </header>
 <!-- END HEADER -->
