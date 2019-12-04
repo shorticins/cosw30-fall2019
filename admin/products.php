d3d4ae62aec8ef918213323f82483c24dad3ce3c
developergrace <gpark2351@gmail.com>
2019-12-04 06:54:30 +0000

    updated products.php -GP

--- admin/products.php
+++ admin/products.php
@@ -1,45 +1,47 @@
 <?php
-include("includes/header.php");
-include("model/product.php");
+    include("includes/header.php");
+    include("model/product.php");
 
-$products = getProducts();
+    $products = getProducts();
 ?>
+
 <main class="col-md-10">
+
 <?php
-if(is_array($products)) {
-    echo "<table class='table'>
-    <thead class='thead-dark'>
-        <tr>
-            <th>Product Id</th>
-            <th>Product Name</th>
-            <th>Product Description</th>
-            <th>Product Price</th>
-            <th>Product Vendor</th>
-            <th>Product Image URL</th>
-            <th>Edit / Delete</th>
-        </tr>
-    </thead>
+    if(is_array($products)) {
+        echo "<table class='table'>
+        <thead class='thead-dark'>
+            <tr>
+                <th>Product Id</th>
+                <th>Product Name</th>
+                <th>Product Description</th>
+                <th>Product Price</th>
+                <th>Product Vendor</th>
+                <th>Product Image URL</th>
+                <th>Edit / Delete</th>
+            </tr>
+        </thead>
 
-    <tbody>";
+        <tbody>";
 
-    foreach($products as $product){
-        echo "<tr><td>".$product['product_id']."</td>";
-        echo "<td>".$product['product_name']."</td>";
-        echo "<td>".$product['product_desc']."</td>";
-        echo "<td>".$product['product_price']."</td>";
-        echo "<td>".$product['product_vendor']."</td>";
-        echo "<td>".$product['product_img_url']."
-        <a href='product.php?id=".$product['product_id']."> Edit </a> 
-         </td></tr>";
+        foreach($products as $product) {
+            echo '<tr>
+                <td>' . $product['product_id'] . '</td>
+                <td>' . $product['product_name'] . '</td>
+                <td>' . $product['product_desc'] . '</td>
+                <td>' . $product['product_price'] . '</td>
+                <td>' . $product['product_vendor'] . '</td>
+                <td>' . $product['product_img_url'] . '</td>
+                <td><a href="product.php?id=' . $product['product_id'] . '">Edit</a></td>
+                </tr>';
+        }
+        echo "</tbody></table>";
+        
+    } else {
+        echo $products;
     }
-    echo "</tbody></table>";
-} else {
-    echo $products;
-}
 ?>
 
 </main>
 
-<?php include("includes/footer.php"); ?>
-
-
+<?php include("includes/footer.php"); ?> 
