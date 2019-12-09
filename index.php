5c60d2cfef5e82434505e354eb2e26f1542f7cc2
shorticins <shanendibbern@gmail.com>
2019-12-09 23:53:03 +0000

    section content added to index.php

--- index.php
+++ index.php
@@ -18,21 +18,21 @@ include('model/product.php');
         <div class="category_item">
             <div class="category_item_img"><a href=""><img src="/img/bone_pile.png" alt="Puppy Bone Cake"></a></div>
             <h3><a href="">Barkery</a></h3>
-            <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
+            <p>The TreatStreet Barkery is committed to providing the best personalized treats for your pet. All of our natural products include real meat and vegetables. We offer grain free options that your pet of any age will savor and enjoy. Treat your pet with one of our barkery items today!</p>
             <a class="btn btn--green">See More</a>
         </div>
 
         <div class="category_item">
             <div class="category_item_img"><a href=""><img src="/img/stuffed_toy.jpg" alt="Stuffed Monkey Toy"></a></div>
             <h3><a href="">Accessories</a></h3>
-            <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
+            <p>Our accessories include durable, fun toys for indoor and outdoor exercise and play, clothing and costumes for your dog or cat and matching collar and leash sets boasting designs for all occasions, keychains, pet name tags and more. Special orders are available upon request.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
         <div class="category_item">
             <div class="category_item_img"><a href=""><img src="/img/rescue_pumpkin.jpg" alt="Pumpkin"></a></div>
             <h3><a href="">Adoptables</a></h3>
-            <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
+            <p>Every pet deserves a forever home and TreatStreet is committed to helping the animals in our adoption center find their family. We currently care for puppies, kittens and adult dogs and cats while they await a new home. Contact us today to discuss the adoption process.</p>
             <a class="btn btn--green">See More</a>
         </div>
     </div>
@@ -52,21 +52,21 @@ include('model/product.php');
         <div class="category_item">
             <div class="category_item_img"><img src="/img/homemade_treats.jpg" alt="Assorted Treats"></div>
             <h3>Dog Treats</h3>
-            <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
+            <p>Organic and nutritious, our dog treats are sure to be a hit with your pet. Our treats are handcrafted, delicious, visually appealing, and perfect for training or otherwise rewarding your dog. We also offer treats for dogs with special dietary needs.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
         <div class="category_item">
             <div class="category_item_img"><img src="/img/dog_treat_cake.jpg" alt="Pup Cake"></div>
             <h3>Treat Cakes</h3>
-            <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
+            <p>Dogs go nuts for our treat cakes and cupcakes. Each cake is designed to light up your dog's life, while maintaining nutritional standards and creating an experience that is both delightful and sought after. We accomodate special orders and instructions upon request.</p>
             <a class="btn btn--green">See More</a>
         </div>
 
         <div class="category_item">
             <div class="category_item_img"><img src="/img/festive_pop.jpg" alt="Pup Pops"></div>
             <h3>Puppy Pops</h3>
-            <p>Borkdrive shoob most angery pupper I have ever seen blep yapper, long doggo vvv big ol pupper. Puggo shooberino, pupperino. porgo. Ruff heckin good boys thicc long bois shoober, you are doing me a frighten wrinkler. Bork borkdrive doing me a frighten waggy wags, pupperino blop. Puggorino you are doin me a concern big ol pupper bork such treat shoob, heck yapper heck very taste wow heckin, sub woofer long woofer shoober long doggo.</p>
+            <p>Our puppy pops are for dogs and puppies alike. As with all of our food products, we offer age based and other diets for your pet's needs. These pops are a delightful reward for dogs and consist of healthy, natural ingredients.</p>
             <a class="btn btn--green">See More</a>
         </div>
     </div>
